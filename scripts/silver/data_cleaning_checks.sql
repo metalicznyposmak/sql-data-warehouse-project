fe459f5a8aab8b6a28d9b15d1eded36506ccf0a5
Wojciech Kucharski <jzacdc@gmail.com>
2025-11-01 14:56:36 +0100

    Updating data clening chekcs

---
+++ scripts/silver/data_cleaning_checks.sql
@@ -0,0 +1,54 @@
+-- Check for nulls and duplicates in Primary Key cst_id in silver.crm_cust_info
+-- Return records with duplicate and null primary keys
+
+SELECT
+prd_id,
+COUNT(*)
+FROM bronze.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+
+
+-- Check for unwanted spaces
+SELECT prd_nm
+FROM bronze.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm);
+
+SELECT DISTINCT prd_line
+FROM bronze.crm_prd_info
+
+
+SELECT
+    prd_id,
+    prd_key,
+    REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+    SUBSTRING(prd_key, 7, LEN(prd_key)) AS [prd_key],
+    prd_nm,
+    ISNULL(prd_cost, 0) AS prd_cost,
+    CASE
+        WHEN UPPER(TRIM(prd_line)) LIKE 'M' THEN 'Mountain'
+        WHEN UPPER(TRIM(prd_line)) LIKE 'R' THEN 'Road'
+        WHEN UPPER(TRIM(prd_line)) LIKE 'S' THEN 'Sport'
+        WHEN UPPER(TRIM(prd_line)) LIKE 'T' THEN 'Touring'
+        ELSE 'Unspecified'
+    END AS prd_line,
+    prd_start_dt,
+    prd_end_dt
+FROM bronze.crm_prd_info
+
+SELECT *
+FROM bronze.crm_prd_info
+WHERE prd_end_dt < prd_start_dt
+
+SELECT
+    prd_id,
+    prd_key,
+    prd_nm,
+    prd_cost,
+    prd_line,
+    prd_start_dt,
+    prd_end_dt,
+LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) AS prd_end_dt_test
+FROM bronze.crm_prd_info
+WHERE prd_key IN ('AC-HE-HL-U509-R', 'AC-HE-HL-U509')
