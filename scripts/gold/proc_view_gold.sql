849b8bbeded03c1b66f5877bda30fa76fef89a96
Wojciech Kucharski <jzacdc@gmail.com>
2025-11-04 12:30:39 +0100

    All the cripts for creating views are ready, scripts integrate the database into 3 views 1 - fact_sales which is a fact table representing sales events, 2 - dim_customers which is a dimension table representing customers 3 - dim_products which is a dimension table for products

--- scripts/gold/proc_view_gold.sql
+++ scripts/gold/proc_view_gold.sql
@@ -1,28 +1,23 @@
-SELECT
-    ci.cst_id,
-    ci.cst_key,
-    ci.cst_firstname,
-    ci.cst_lastname,
-    ci.cst_marital_status,
-    cst_gndr,
-    CASE
-        WHEN ci.cst_gndr 
-    eca.bdate,
-    eca.gen,
-    ela.cntry
-FROM
-    silver.crm_cust_info ci 
-    LEFT JOIN silver.erp_cust_az12 eca
-    ON ci.cst_key = eca.cid
-    LEFT JOIN silver.erp_loc_a101 ela
-    ON ci.cst_key = ela.cid
-
-SELECT DISTINCT
-    cst_gndr,
-    eca.gen
-FROM
-    silver.crm_cust_info ci 
-    LEFT JOIN silver.erp_cust_az12 eca
-    ON ci.cst_key = eca.cid
-    LEFT JOIN silver.erp_loc_a101 ela
-    ON ci.cst_key = ela.cid
+CREATE VIEW gold.dim_customers AS
+    SELECT
+        ROW_NUMBER() OVER (ORDER BY ci.cst_id) AS customer_key,
+        ci.cst_id AS customer_id,
+        ci.cst_key AS customer_nummber,
+        ci.cst_firstname AS first_name,
+        ci.cst_lastname AS last_name,
+        ela.cntry AS country,
+        ci.cst_marital_status AS marital_status,
+        CASE
+            WHEN eca.gen IS NULL THEN ci.cst_gndr
+            WHEN ci.cst_gndr LIKE 'n/a' THEN eca.gen -- CRM is the master for gender info
+            ELSE ci.cst_gndr
+        END AS gender,
+        eca.bdate AS birth_date,
+        ci.cst_create_date AS create_date
+    FROM
+        silver.crm_cust_info ci 
+        LEFT JOIN silver.erp_cust_az12 eca
+        ON ci.cst_key = eca.cid
+        LEFT JOIN silver.erp_loc_a101 ela
+        ON ci.cst_key = ela.cid;
+    SIEMA ENIU
